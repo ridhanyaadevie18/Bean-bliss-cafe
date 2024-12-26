e19f4399b97a89eb4d7e58d50c92acfe5e2b2ee3
ridhanyaadevie18 <ridhanyaadevie.2024cse@sece.ac.in>
2024-12-26 16:45:50 +0000

    feat:added javascript part

---
+++ coffee.js
@@ -0,0 +1,35 @@
+let navbar= document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+
+navbar.classList.toggle('active');
+
+searchForm.classList.remove('active');
+
+cartItem.classList.remove('active');
+
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+
+cartItem.classList.remove('active');
+
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+
+ searchForm.classList.remove('active');
+cartItem.classList.toggle('active');
+
+}
+window.onscroll = () => {
+
+    navbar.classList.remove('active');
+    
+    searchForm.classList.remove('active');
+    
+    cartItem.classList.remove('active');}
